4f64096c24b90f02b302eb79483cd5ee87f1c27e
Amim <amim.knabben@gmail.com>
2017-02-05 20:56:34 -0200

    Requirements from oauth2, commenting this capability for now

--- terminator/terminator/urls.py
+++ terminator/terminator/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     # APIs
     url('items/', views.ListItem.as_view(), name="items"),
 
+    # Authentication namespace
     url('^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+
     url(r'^admin/', admin.site.urls),
 ]
